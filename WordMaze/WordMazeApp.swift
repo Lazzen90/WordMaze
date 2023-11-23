3e38aa0e1c60b6b39d7fc945010108c23b91db8e
Jacob Lassen <lassen90@gmail.com>
2023-11-23 12:31:29 +0100

    Initial Commit

---
+++ WordMaze/WordMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordMazeApp.swift
+//  WordMaze
+//
+//  Created by Jacob Lassen on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
